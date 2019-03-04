82265822f6322528e5d4ef512152605369266466
Jay Piamjariyakul <j.piamjariyakul@outlook.com>
2019-03-04 18:00:04 +0000

    Port-mapped components
    Attempted port-mapping of components - not tested if correct
    
    Based on SDR lab's code regarding port-mapping

--- Vivado/190302_digi_cmd_interim_01.srcs/sources_1/imports/190302_digi_cmd_interim_01/cmdProc.vhd
+++ Vivado/190302_digi_cmd_interim_01.srcs/sources_1/imports/190302_digi_cmd_interim_01/cmdProc.vhd
@@ -13,42 +13,83 @@ use work.common_pack.all;
 
 -- 190304: Defined entity
 entity cmdProc is
-    port (
-        -- Ports with no links to any other modules
-        clk:                in std_logic;
-        reset:              in std_logic;                               -- synchronous reset
-        
-        -- Ports linked to data processor            
-        dataReady:          in std_logic;
-        byte:               in std_logic_vector(7 downto 0);
-        maxIndex:           in BCD_ARRAY_TYPE(2 downto 0);
-        dataResults:        in CHAR_ARRAY_TYPE(0 to RESULT_BYTE_NUM-1); -- index 3 holds the peak
-        seqDone:            in std_logic;
-        start:              out std_logic;                              -- goes high to signal data transfer
-        numWords_bcd:       out BCD_ARRAY_TYPE(2 downto 0);
-        
-        -- Ports linked to Rx
-        rxDone:             in std_logic;                               -- data succesfully read (active high)
-        rcvDataReg:         out std_logic_vector(7 downto 0);           -- received data
-        dataValid:          out std_logic;                              -- data ready to be read (called dataReady in Rx.VHD)
-        setOE:              out std_logic;                              -- overrun error (active high)
-        setFE:              out std_logic;                              -- frame error (active high)
-                    
-        -- Ports linked to Tx
-        READY:              in  STD_LOGIC;                              -- equivalent to txDone
-        SEND:               out  STD_LOGIC;                             -- equivalent to txNow
-        DATA:               out  STD_LOGIC_VECTOR (7 downto 0)
-    );
+	port (
+		-- Ports with no links to any other modules
+		clk:		in	std_logic;
+		reset:		in	std_logic;					-- synchronous reset
+		
+		-- Ports linked to data processor            
+		dataReady:	in	std_logic;
+		byte:		in	std_logic_vector(7 downto 0);
+		maxIndex:	in	BCD_ARRAY_TYPE(2 downto 0);
+		dataResults:	in	CHAR_ARRAY_TYPE(0 to RESULT_BYTE_NUM-1);	-- index 3 holds the peak
+		seqDone:	in	std_logic;
+		start:		out	std_logic;					-- goes high to signal data transfer
+		numWords_bcd:	out	BCD_ARRAY_TYPE(2 downto 0);
+		
+		-- Ports linked to Rx
+		rxDone:		in	std_logic;					-- data succesfully read (active high)
+		rcvDataReg:	out	std_logic_vector(7 downto 0);			-- received data
+		dataValid:	out	std_logic;					-- data ready to be read (called dataReady in Rx.VHD)
+		setOE:		out	std_logic;					-- overrun error (active high)
+		setFE:		out	std_logic;					-- frame error (active high)
+			    
+		-- Ports linked to Tx
+		READY:		in	STD_LOGIC;					-- equivalent to txDone
+		SEND:		out	STD_LOGIC;					-- equivalent to txNow
+		DATA:		out	STD_LOGIC_VECTOR (7 downto 0)
+	);
 end cmdProc;
 
 -- 190304: Added arch - TBC
 architecture cmdProc_arch of cmdProc is
-    component shiftd port (
-        din: in std_logic; -- DATA IN
-        en: in std_logic; -- CHIP ENABLE
-        clk: in std_logic; -- CLOCK
-        y: out std_logic_vector(7 downto 0); -- SHIFTER OUTPUT
-        dirup: in std_logic -- SHIFT DIRECTION
-        );
-    end component;
+	component UART_RX_CTRL
+		port (
+			RxD:		in	std_logic;					-- serial data in
+			sysclk:		in	std_logic;					-- system clock
+			reset:		in	std_logic;					-- synchronous reset
+			rxDone:		in	std_logic;					-- data succesfully read (active high)
+		  	rcvDataReg:	out	std_logic_vector(7 downto 0);			-- received data
+			dataReady:	out	std_logic;					-- data ready to be read
+			setOE:		out	std_logic;					-- overrun error (active high)
+			setFE:		out	std_logic					-- frame error (active high)
+		);
+	end component;
+	
+	component UART_TX_CTRL 
+		port (
+			SEND:		in	STD_LOGIC;
+			DATA: 		in	STD_LOGIC_VECTOR (7 downto 0);
+			CLK:		in	STD_LOGIC;
+			READY:		out	STD_LOGIC;
+			UART_TX:	out	STD_LOGIC
+		);
+	end component;
+	
+	component dataConsume
+		port (
+			clk:		in	std_logic;
+		  	reset:		in	std_logic; -- synchronous reset
+		  	start:		in	std_logic; -- goes high to signal data transfer
+		  	numWords_bcd:	in	BCD_ARRAY_TYPE(2 downto 0);
+			ctrlIn:		in	std_logic;
+		  	data:		in	std_logic_vector(7 downto 0);
+			ctrlOut:	out	std_logic;
+			dataReady:	out	std_logic;
+			byte:		out	std_logic_vector(7 downto 0);
+			seqDone:	out	std_logic;
+			maxIndex:	out	BCD_ARRAY_TYPE(2 downto 0);
+			dataResults:	out	CHAR_ARRAY_TYPE(0 to RESULT_BYTE_NUM-1) -- index 3 holds the peak
+		);
+	end component;
+    
+begin
+    
+--    delay_CtrlIn: process(clk)     
+--    begin
+--        if rising_edge(clk) then
+
+--        end if;
+--    end process;
+    
 end cmdProc_arch;
