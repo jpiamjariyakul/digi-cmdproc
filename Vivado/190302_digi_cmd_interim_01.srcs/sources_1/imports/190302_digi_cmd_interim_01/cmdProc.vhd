f1b2698a0b25c7c412c7363f756a9a7f511022be
Jay Piamjariyakul <j.piamjariyakul@outlook.com>
2019-03-17 21:37:40 +0000

    Included rxNow in FSM

--- Vivado/190302_digi_cmd_interim_01.srcs/sources_1/imports/190302_digi_cmd_interim_01/cmdProc.vhd
+++ Vivado/190302_digi_cmd_interim_01.srcs/sources_1/imports/190302_digi_cmd_interim_01/cmdProc.vhd
@@ -10,6 +10,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use work.common_pack.all;
+use IEEE.numeric_std.ALL;
 
 -- 190304: Defined entity
 entity cmdProc is
@@ -20,7 +21,7 @@ port (
     -- I/O of Rx module
     rxdone:         out     std_logic; -- Same as "done" in diagram
     rxData:         in      std_logic_vector (7 downto 0);
-	rxnow:          in      std_logic; -- Same as "valid" in diagram
+	rxNow:          in      std_logic; -- Same as "valid" in diagram
     ovErr:          in      std_logic; 
     framErr:        in      std_logic;
     -- I/O of Tx module
@@ -40,13 +41,16 @@ end cmdProc;
 -- 190304: Added arch - TBC
 architecture cmdProc_behav of cmdProc is
 -- Component declaration of dataConsume
-    TYPE state_type IS (INIT, valid_A, valid_1, valid_2); -- List your states here 
+    TYPE state_type IS (INIT, dataValid, valid_A, valid_1, valid_2, data); -- List your states here 
 	SIGNAL curState, nextState : state_type;
 	SIGNAL processed : BIT; -- registered input signal
 	SIGNAL rxData_reg: std_logic_vector(7 downto 0); -- Stores rxData into FF
 	--SIGNAL num1, num2, num3: std_logic_vector(7 downto 0); --  Stores numbers inputted
 	SIGNAL en_count_byte: BIT; -- ENABLE input for counter 
-	SIGNAL count_byte: INTEGER := 0; -- counter integers for bytes retrieved
+	--SIGNAL count_byte: INTEGER := 0; -- counter integers for bytes retrieved
+	SIGNAL count_byte: INTEGER := 0; --BCD_ARRAY_TYPE(2 downto 0):= ("0000", "0000", "0000"); -- counter integers for bytes retrieved
+	SIGNAL bcd_integer, bcd_2, bcd_1, bcd_0: INTEGER; -- Equivalent integer
+	SIGNAL num_bcd: BCD_ARRAY_TYPE(2 downto 0) := ("0000", "0000", "0000"); -- Stores the same value as numWords_bcd, but can be read
 begin
 	-- 190310: IDEA - instead of using processed FF, output invalid data instead?
 	-- 190311:	Sequence working! (sans L & P)
@@ -68,12 +72,31 @@ begin
 			END IF;
 		END IF;
 	END PROCESS;
+	
+--	PROCESS (clk, curState, num_bcd, bcd_integer, bcd_2, bcd_1, bcd_0)
+--	BEGIN
+--		IF clk'EVENT AND clk = '1' THEN
+--			bcd_2 <= to_integer(unsigned(num_bcd(2)));
+--			bcd_1 <= to_integer(unsigned(num_bcd(1)));
+--			bcd_0 <= to_integer(unsigned(num_bcd(0)));
+--			bcd_integer <= (100 * bcd_2) + (10 * bcd_1) + bcd_0;
+--		END IF;
+--	END PROCESS;
 	----------------------------------------
-	combi_nextState : PROCESS (curState, rxData, processed)
+	combi_nextState : PROCESS (curState, rxNow, rxData, processed, count_byte, num_bcd, bcd_integer, bcd_2, bcd_1, bcd_0)
+		variable v_rxDone: std_logic; -- variable for rxDone
 	BEGIN
+		v_rxDone := '0'; -- Default value of rxDone
 		CASE curState IS
-			WHEN INIT => 
-				IF (rxData = "01000001") or (rxData = "01100001") THEN
+--			WHEN INIT =>
+--				IF (rxnow = '1') THEN
+--					nextState <= dataValid;
+--				ELSE
+--					nextState <= INIT;
+--				END IF;
+			
+			WHEN INIT => --(sig_rxNow = '1') and 
+				IF (rxNow = '1') and ((rxData = "01000001") or (rxData = "01100001")) THEN
 					nextState <= valid_A;
 				ELSE
 					nextState <= INIT;
@@ -81,7 +104,8 @@ begin
  
 			WHEN valid_A => 
 				IF (rxData /= rxData_reg) and ((rxData = "00110000") OR (rxData = "00110001") OR (rxData = "00110010") OR (rxData = "00110011") OR (rxData = "00110100") OR (rxData = "00110101") OR (rxData = "00110110") OR (rxData = "00110111") OR (rxData = "00111000") OR (rxData = "00111001")) THEN -- 0
-					numWords_bcd(2) <= rxData(3 downto 0);
+					num_bcd(2) <= rxData(3 downto 0);
+					numWords_bcd(2) <= num_bcd(2);
 					nextState <= valid_1;
 					--ELSIF (rxData = "01000001") or (rxData = "01100001") THEN
 						--nextState <= INIT;
@@ -95,7 +119,8 @@ begin
 				IF (rxData = rxData_reg) THEN
 					nextState <= valid_1;
 				ELSIF (rxData /= rxData_reg) and ((rxData = "00110000") OR (rxData = "00110001") OR (rxData = "00110010") OR (rxData = "00110011") OR (rxData = "00110100") OR (rxData = "00110101") OR (rxData = "00110110") OR (rxData = "00110111") OR (rxData = "00111000") OR (rxData = "00111001")) THEN -- 0
-					numWords_bcd(1) <= rxData(3 downto 0);
+					num_bcd(1) <= rxData(3 downto 0);
+					numWords_bcd(1) <= num_bcd(1);
 					nextState <= valid_2;
 				ELSIF (rxData = "01000001") or (rxData = "01100001") THEN
 					nextState <= valid_A;
@@ -107,8 +132,17 @@ begin
 				IF (rxData = rxData_reg) THEN
 					nextState <= valid_2;
 				ELSIF (rxData /= rxData_reg) and ((rxData = "00110000") OR (rxData = "00110001") OR (rxData = "00110010") OR (rxData = "00110011") OR (rxData = "00110100") OR (rxData = "00110101") OR (rxData = "00110110") OR (rxData = "00110111") OR (rxData = "00111000") OR (rxData = "00111001")) THEN -- 0
-					numWords_bcd(0) <= rxData(3 downto 0);
-					nextState <= INIT;
+					num_bcd(0) <= rxData(3 downto 0);
+					numWords_bcd(0) <= num_bcd(0);
+					
+					-- Converts total value into BCD
+					bcd_2 <= to_integer(unsigned(num_bcd(2)));
+					bcd_1 <= to_integer(unsigned(num_bcd(1)));
+					bcd_0 <= to_integer(unsigned(num_bcd(0)));
+					bcd_integer <= (100 * bcd_2) + (10 * bcd_1) + bcd_0;
+					
+					v_rxDone := '1'; -- Sets rxDone high for 1 clkCycle
+					nextState <= data;
 				ELSIF (rxData = "01000001") or (rxData = "01100001") THEN
 					nextState <= valid_A;
 				ELSE
@@ -116,9 +150,17 @@ begin
 				END IF;
 				-- Question 15: Added an "others" statement to case selection
 				-- Given such, when an invalid encoding is given, the program jumps its state to INIT
+			WHEN data =>
+				-- TODO: Compare numWords_bcd w/ count_byte
+				IF (count_byte < (bcd_integer - 1)) THEN
+					nextState <= data;
+				ELSE
+					nextState <= INIT;
+				END IF;
 			WHEN OTHERS => 
 				nextState <= INIT;
 		END CASE;
+		rxDone <= v_rxDone;
 	END PROCESS; -- combi_nextState
 	-----------------------------------------------------
 	seq_state : PROCESS (clk, reset)
@@ -130,21 +172,24 @@ begin
 		END IF;
 	END PROCESS; -- seq
 	-----------------------------------------------------
-	run_ANNN: PROCESS(curState) -- Used for linking data processing
+	run_ANNN: PROCESS(curState, seqDone, count_byte, bcd_integer) -- Used for linking data processing
 	BEGIN
 	-- 190316-01: Began coding for aNNN (Jay)
 	-- Default values for "start"
-	start <= '0';
+		start <= '0';
 		if (curState = valid_2) and (rxData /= rxData_reg) and ((rxData = "00110000") OR (rxData = "00110001") OR (rxData = "00110010") OR (rxData = "00110011") OR (rxData = "00110100") OR (rxData = "00110101") OR (rxData = "00110110") OR (rxData = "00110111") OR (rxData = "00111000") OR (rxData = "00111001")) THEN
-			start <= '1';
+			start <= '1'; -- starts data retrieval
+		elsif (curState = data) and (seqDone = '1') then
+			start <= '1'; -- stops data retrieval
 		end if;
 	END PROCESS;
 	-----------------------------------------------------
 	-- Enable for counter
-	enable_byte : PROCESS (reset, clk)
+	enable_byte : PROCESS (reset, clk, count_byte, bcd_integer)
 	BEGIN
 		en_count_byte <= '0'; -- assign default value
-		IF (curState = valid_2) and (rxData /= rxData_reg) and ((rxData = "00110000") OR (rxData = "00110001") OR (rxData = "00110010") OR (rxData = "00110011") OR (rxData = "00110100") OR (rxData = "00110101") OR (rxData = "00110110") OR (rxData = "00110111") OR (rxData = "00111000") OR (rxData = "00111001")) THEN
+		--IF (curState = valid_2) and (rxData /= rxData_reg) and ((rxData = "00110000") OR (rxData = "00110001") OR (rxData = "00110010") OR (rxData = "00110011") OR (rxData = "00110100") OR (rxData = "00110101") OR (rxData = "00110110") OR (rxData = "00110111") OR (rxData = "00111000") OR (rxData = "00111001")) THEN
+		IF (curState = data) AND (count_byte < (bcd_integer - 1)) THEN
 			en_count_byte <= '1';
 		END IF;
 	END PROCESS;
@@ -153,7 +198,7 @@ begin
 	counter_byte : PROCESS (reset, clk, en_count_byte)
 	BEGIN
 		IF reset = '0' THEN -- active high reset
-			count_byte <= 0;
+			count_byte <= 0; --("0000", "0000", "0000");
 		ELSIF clk'EVENT AND clk = '1' THEN
 			IF en_count_byte = '1' THEN
 				-- Counter for 0s only counts in INIT or bit_0s states & when registered input is 0
